268113c999dc4909a68cc3d62aec9646103b923e
robotseth <thebonsaidude@gmail.com>
2022-04-02 14:39:26 -0700

    High/Low Pass filters work | exports work

--- filter_plot.py
+++ filter_plot.py
@@ -6,8 +6,7 @@
 
 import numpy as np
 import scipy as sp
-from scipy.io.wavfile import read
-from scipy.io.wavfile import write     # Imported libaries such as numpy, scipy(read, write), matplotlib.pyplot
+from scipy.io.wavfile import read, write
 from scipy import signal
 import matplotlib.pyplot as plt
 # get_ipython().magic('matplotlib inline')
@@ -17,20 +16,39 @@ import matplotlib.pyplot as plt
 
 
 # Replace this with the location of your downloaded file.
-(Frequency, array) = read('C:\\Users\\Seth\\Documents\\school\\EGR334\\audio\\Lacrimosa.wav') # Reading the sound file.
+
+
+#(samplerate, array) = read('C:\\Users\\Seth\\Documents\\school\\EGR334\\audio\\Lacrimosa.wav') # Reading the sound file.
+(samplerate, array) = read('C:\\Users\\Seth\\Documents\\school\\EGR334\\audio\\Chicago.wav') # Reading the sound file.
+
+
 #(Frequency, array) = read('C:\\Users\\mikef\\Desktop\\_Spring 2022\\EGR 334 T 9am\\Final Project\\5. Harris Heller - Floating Soul.wav') # Reading the sound file.
 
 
 # In[11]:
+print(f"number of channels = {array.shape[1]}")
+length = array.shape[0] / samplerate
+print(f"length = {length}s")
 
-
-len(array) # length of our array
-array = array[0:len(array),1]
+time = np.linspace(0., length, array.shape[0])
+"""
+plt.plot(time, array[:, 0], label="Left channel")
+plt.plot(time, array[:, 1], label="Right channel")
+plt.legend()
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude")
+plt.show()
+"""
+left = array[:, 0]
+left_scaled = np.int16(left/np.max(np.abs(left)) * 32767)
+write("OriginalAudio.wav", samplerate, left_scaled) # Saving it to the file.
 
 # In[12]:
 
 
-plt.plot(array)
+#### always at zero, why? ####
+
+plt.plot(left_scaled,'b')
 plt.title('Original Signal Spectrum')
 plt.xlabel('Frequency(Hz)')
 plt.ylabel('Amplitude')
@@ -39,24 +57,26 @@ plt.ylabel('Amplitude')
 # In[13]:
 
 
-FourierTransformation = sp.fft.fft(array) # Calculating the fourier transformation of the signal
+#len(array) # length of our array
+#left = array[0:len(left),1]
+
+
+FourierTransformation = sp.fft.fft(left) # Calculating the fourier transformation of the left signal
 
 
 # In[14]:
 
 
-scale = np.linspace(0, Frequency, len(array))
-print(scale)
+#scale = np.linspace(0, samplerate, left)
+#print(scale)
 
 # In[15]:
-
-
-plt.stem(scale[0:5000], np.abs(FourierTransformation[0:5000]))  # The size of our diagram
+'''
+plt.stem(scale[0:5000], np.abs(FourierTransformation[0:5000]),'r')  # The size of our diagram
 plt.title('Signal spectrum after FFT')
 plt.xlabel('Frequency(Hz)')
 plt.ylabel('Amplitude')
-
-
+'''
 # In[16]:
 
 
@@ -66,50 +86,58 @@ GuassianNoise = np.random.rand(len(FourierTransformation)) # Adding guassian Noi
 # In[17]:
 
 
-NewSound = GuassianNoise + array
+#NewSound = GuassianNoise + left
+NewSound = left
 
 
 # In[18]:
-
-
-write("New-Sound-Added-With-Guassian-Noise.wav", Frequency, NewSound) # Saving it to the file.
+NewSound_scaled = np.int16(NewSound/np.max(np.abs(NewSound)) * 32767)
+write("New-Sound-Added-With-Guassian-Noise.wav", samplerate, NewSound_scaled) # Saving it to the file.
 
 
 # In[19]:
 
 
-b,a = signal.butter(5, 1000/(Frequency/2), btype='highpass') # ButterWorth filter 4350
+b,a = signal.butter(5, 1000/(samplerate/2), btype='highpass') # ButterWorth filter 4350
 
 
 # In[20]:
 
 
-filteredSignal = signal.lfilter(b,a,NewSound)
-plt.plot(filteredSignal) # plotting the signal.
+filteredSignal = signal.lfilter(b,a,NewSound_scaled)
+"""
+plt.plot(filteredSignal,'g', alpha=0.25) # plotting the signal.
 plt.title('Highpass Filter')
 plt.xlabel('Frequency(Hz)')
 plt.ylabel('Amplitude')
-
-
+"""
 # In[21]:
 
 
-c,d = signal.butter(5, 380/(Frequency/2), btype='lowpass') # ButterWorth low-filter
+c,d = signal.butter(5, 380/(samplerate/2), btype='lowpass') # ButterWorth low-filter
 newFilteredSignal = signal.lfilter(c,d,filteredSignal) # Applying the filter to the signal
-plt.plot(newFilteredSignal) # plotting the signal.
+"""
+plt.plot(newFilteredSignal,'k',alpha=0.25) # plotting the signal.
+plt.title('Lowpass Filter')
+plt.xlabel('Frequency(Hz)')
+plt.ylabel('Amplitude')
+"""
+
+# In[22]:
+filteredSignal_scaled = np.int16(filteredSignal/np.max(np.abs(filteredSignal)) * 32767)
+newFilteredSignal_scaled = np.int16(newFilteredSignal/np.max(np.abs(newFilteredSignal)) * 32767)
+
+plt.plot(newFilteredSignal_scaled,'r',alpha=0.25) # plotting the signal.
 plt.title('Lowpass Filter')
 plt.xlabel('Frequency(Hz)')
 plt.ylabel('Amplitude')
 
-
-# In[22]:
-
-
-write("New-Filtered-Eagle-Sound.wav", Frequency, newFilteredSignal) # Saving it to the file.
+write("Filtered-Eagle-Sound.wav", samplerate, filteredSignal_scaled) # Saving it to the file.
 
 # %%
 b, a = signal.butter(4, 100, 'low', analog=True)
 w, h = signal.freqs(b, a)
+"""
 plt.plot(w, 20 * np.log10(abs(h)))
 plt.xscale('log')
 plt.title('Butterworth filter frequency response')
@@ -118,5 +146,8 @@ plt.ylabel('Amplitude [dB]')
 plt.margins(0, 0.1)
 plt.grid(which='both', axis='both')
 plt.axvline(100, color='green') # cutoff frequency
+
+"""
 plt.show()
+
 # %%
