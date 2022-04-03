7bf6724a4b1add044934069bd62636de66931a44
robotseth <thebonsaidude@gmail.com>
2022-04-02 22:26:28 -0700

    can play multiple chunks with threads

--- process_audio.py
+++ process_audio.py
@@ -3,6 +3,9 @@ import os
 import scipy as sp
 from scipy.io.wavfile import read, write
 from scipy import signal
+import sounddevice as sd
+import threading
+import time
 
 # create chunk object from input (sensor reading or audio file)
 # filter chunk and export 3 new chunks (low, mid, high)
@@ -29,26 +32,23 @@ class Chunk:
 
 # split audio file into chunks
 def import_wav (file_path, chunk_size):
-    global chunk_array
+    chunk_array = []
     scaled_data = 0
     (sample_rate, raw_array) = read(file_path) # reads the wave file into an array
     raw_length_ms = 1000 * raw_array.shape[0] / sample_rate
     num_chunks = int(raw_length_ms / chunk_size) + (raw_length_ms % chunk_size > 0)
-    print(raw_length_ms)
-    print(num_chunks)
+    print("Length (ms): " + str(raw_length_ms))
     left_data = raw_array[:, 0]
     scaled_data = np.int16(left_data / np.max(np.abs(left_data)) * 32767)
     sub_arrays = np.array_split(scaled_data, num_chunks)
-    #print(sub_arrays)
     for i in range(num_chunks):
         chunk = Chunk()
         chunk.data = sub_arrays[i]
         chunk.sample_rate = sample_rate
-        chunk.length = len(sub_arrays[i])
+        chunk.length = raw_length_ms #len(sub_arrays[i])
         chunk_array.append(chunk)
-    print(len(chunk_array))
-    #print(chunk_array[0].data)
-# create array of chunks
+    print("Number of chunks: " + str(len(chunk_array)))
+    return chunk_array
 
 def filter (chunk, type):
     if type == "highpass" or type == "h" :
@@ -75,60 +75,38 @@ def export_chunk (chunk, number, name):
     write_address = os.path.join("C:\\Users\\Seth\\Documents\\school\\EGR334\\exports\\", filename)
     write(write_address, chunk_array[number].sample_rate, chunk)  # Saving it to the file.
 
+def play_chunk (chunk, sample_rate, audio_device):
+        sd.play(chunk.data, sample_rate, device=4)
+        #stream = sd.OutputStream(samplerate=chunk.sample_rate, device=audio_device, channels=1)
+        sd.wait()
+
+def play_chunks (chunk_array, sample_rate, audio_device):
+    for n in range(len(chunk_array)):
+        play_chunk(chunk_array[n], sample_rate, audio_device)
+
 if __name__ == '__main__':
-    chunk_array = []
-    import_wav('C:\\Users\\Seth\\Documents\\school\\EGR334\\audio\\Chicago.wav', 10000)
+    threads = []  # list to hold threads
+    chunk_array = import_wav('C:\\Users\\Seth\\Documents\\school\\EGR334\\audio\\Chicago.wav', 10000)
+    high_chunks = []
+    band_chunks = []
+    low_chunks = []
     for n in range(len(chunk_array)):
         [high_chunk, band_chunk, low_chunk] = filter_chunk(chunk_array[n])
-        export_chunk(high_chunk,n,"high")
-        export_chunk(band_chunk, n,"band")
-        export_chunk(low_chunk, n,"low")
+        high_chunks.append(high_chunk)
+        band_chunks.append(band_chunk)
+        low_chunks.append(low_chunk)
+    print(len(high_chunks))
+    low = threading.Thread(target=play_chunks, args=(low_chunks, chunk_array[n].sample_rate, 4))
+    threads.append(low)
+    band = threading.Thread(target=play_chunks, args=(band_chunks, chunk_array[n].sample_rate, 4))
+    threads.append(band)
+    high = threading.Thread(target=play_chunks, args=(high_chunks, chunk_array[n].sample_rate, 4))
+    threads.append(high)
+    for thread in threads:
+        thread.start()
+    for thread in threads:  # wait for all threads to finish
+        thread.join()
 
-"""
-print(f"number of channels = {array.shape[1]}")
-length = array.shape[0] / samplerate
-print(f"length = {length}s")
-
-time = np.linspace(0., length, array.shape[0])
-
-# Only uses left channel
-left = array[:, 0]
-left_scaled = np.int16(left/np.max(np.abs(left)) * 32767)
-write("OriginalAudio.wav", samplerate, left_scaled) # Saving it to the file.
-
-FourierTransformation = sp.fft.fft(left) # Calculating the fourier transformation of the left signal
-
-GuassianNoise = np.random.rand(len(FourierTransformation)) # Adding guassian Noise to the signal.
-
-NewSound = left
-
-# Scales the sound to to 16 bit
-NewSound_scaled = np.int16(NewSound/np.max(np.abs(NewSound)) * 32767)
-write("New-Sound-Added-With-Guassian-Noise.wav", samplerate, NewSound_scaled) # Saving it to the file.
-
-##### Highpass Filter #####
-b,a = signal.butter(5, 1000/(samplerate/2), btype='highpass') # ButterWorth filter 4350
-high_data = signal.lfilter(b,a,NewSound_scaled)
-
-
-
-##### Lowpass Filter #####
-c,d = signal.butter(5, 380/(samplerate/2), btype='lowpass') # ButterWorth low-filter
-low_data = signal.lfilter(c,d,NewSound_scaled) # Applying the filter to the signal
-
-##### Bandpass Filter #####
-e,f = signal.butter(5, [380/(samplerate/2),1000/(samplerate/2)], btype='band') # ButterWorth low-filter
-band_data = signal.lfilter(e,f,NewSound_scaled) # Applying the filter to the signal
-
-# scales all signals to 16 bit
-high_scaled = np.int16(high_data/np.max(np.abs(high_data)) * 32767)
-low_scaled = np.int16(low_data/np.max(np.abs(low_data)) * 32767)
-band_scaled = np.int16(band_data/np.max(np.abs(band_data)) * 32767)
-
-
-
-######### Saves Filtered Files #########
-#write("HighSound.wav", samplerate, high_scaled) # Saving it to the file.
-#write("LowSound.wav", samplerate, low_scaled) # Saving it to the file.
-#write("BandSound.wav", samplerate, band_scaled) # Saving it to the file.
-"""
+        # export_chunk(high_chunk,n,"high")
+        # export_chunk(band_chunk, n,"band")
+        # export_chunk(low_chunk, n,"low")
