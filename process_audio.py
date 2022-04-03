ab1610500f042b8384a3c0d69db9467964a721e7
robotseth <thebonsaidude@gmail.com>
2022-04-02 20:53:24 -0700

    makes chunks

--- process_audio.py
+++ process_audio.py
@@ -12,8 +12,7 @@ class Chunk:
     'Common base class for Chunks'
     chunk_count = 0
 
-    def __init__(self, name, length, sample_rate, data):
-        self.name = name
+    def __init__(self, length=None, sample_rate=None, data=None):
         self.length = length
         self.sample_rate = sample_rate
         self.data = data
@@ -24,12 +23,38 @@ class Chunk:
         "Total Chunks %d" % Chunk.chunk_count
 
 #(samplerate, array) = read('C:\\Users\\Seth\\Documents\\school\\EGR334\\audio\\Lacrimosa.wav') # Reading the sound file.
-(samplerate, array) = read('C:\\Users\\Seth\\Documents\\school\\EGR334\\audio\\Chicago.wav') # Reading the sound file.
+#(samplerate, raw_array) = read('C:\\Users\\Seth\\Documents\\school\\EGR334\\audio\\Chicago.wav') # Reading the sound file.
 #(Frequency, array) = read('C:\\Users\\mikef\\Desktop\\_Spring 2022\\EGR 334 T 9am\\Final Project\\5. Harris Heller - Floating Soul.wav') # Reading the sound file.
 
 # split audio file into chunks
+def import_wav (file_path, chunk_size):
+    global chunk_array
+    scaled_data = 0
+    (sample_rate, raw_array) = read(file_path) # reads the wave file into an array
+    raw_length_ms = 1000 * raw_array.shape[0] / sample_rate
+    num_chunks = int(raw_length_ms / chunk_size) + (raw_length_ms % chunk_size > 0)
+    print(raw_length_ms)
+    print(num_chunks)
+    left_data = raw_array[:, 0]
+    scaled_data = np.int16(left_data / np.max(np.abs(left_data)) * 32767)
+    sub_arrays = np.array_split(scaled_data, num_chunks)
+    #print(sub_arrays)
+    for i in range(num_chunks):
+        chunk = Chunk()
+        chunk.data = sub_arrays[i]
+        chunk.sample_rate = sample_rate
+        chunk.length = len(sub_arrays[i])
+        chunk_array.append(chunk)
+    print(len(chunk_array))
+    #print(chunk_array[0].data)
 # create array of chunks
 
+if __name__ == '__main__':
+    chunk_array = []
+    import_wav('C:\\Users\\Seth\\Documents\\school\\EGR334\\audio\\Chicago.wav', 10000)
+    #print(chunk_array[0].data)
+    write("FirstChunkAudio.wav", chunk_array[0].sample_rate, chunk_array[0].data)  # Saving it to the file.
+"""
 print(f"number of channels = {array.shape[1]}")
 length = array.shape[0] / samplerate
 print(f"length = {length}s")
@@ -76,3 +101,4 @@ band_scaled = np.int16(band_data/np.max(np.abs(band_data)) * 32767)
 #write("HighSound.wav", samplerate, high_scaled) # Saving it to the file.
 #write("LowSound.wav", samplerate, low_scaled) # Saving it to the file.
 #write("BandSound.wav", samplerate, band_scaled) # Saving it to the file.
+"""
