329c8ed14510a5268dd2670b85e9b2dc252d3f12
robotseth <thebonsaidude@gmail.com>
2022-04-03 13:55:18 -0700

    Failed attempt to live update chunk arrays

--- process_audio.py
+++ process_audio.py
@@ -45,6 +45,22 @@ def import_wav (file_path, chunk_size):
     print("Number of chunks: " + str(len(chunk_array)))
     return chunk_array
 
+
+
+""""############### NOTES / TO DO ###############"""
+# MAIN LOGIC:
+# each thread should start with a loop that constantly checks if new data has been added to an array of chunks.
+# If it has, it should play the new chunks until it is out of data at which point it loops.
+# The main loop should process an audio file for testing and will read sensor data in the real thing
+
+# program a function that allows the playback threads to only play up until a certain number of chunks back so that a
+# delay can be added for the live sensor reading
+
+# look into not using play and trying stream or something so that there are not hickups on playback.
+# Can you append to the playback array mid-playback?
+
+
+
 def filter (chunk, type):
     if type == "highpass" or type == "h" :
         b, a = signal.butter(5, 1000 / (chunk.sample_rate / 2), btype='highpass')  # ButterWorth filter 4350
@@ -56,13 +72,14 @@ def filter (chunk, type):
         print("Error - not valid filter type")
     data = signal.lfilter(b, a, chunk.data)
     data = np.int16(data / np.max(np.abs(data)) * 32767)
-    return data
+    filtered_chunk = Chunk(chunk.length, chunk.sample_rate, data)
+    return filtered_chunk
 
 def filter_chunk (chunk):
-    high_chunk = filter(chunk,'h')
-    band_chunk = filter(chunk,'b')
-    low_chunk = filter(chunk,'l')
-    filtered_chunks = [high_chunk, band_chunk, low_chunk]
+    h_chunk = filter(chunk,'h')
+    b_chunk = filter(chunk,'b')
+    l_chunk = filter(chunk,'l')
+    filtered_chunks = [h_chunk, b_chunk, l_chunk]
     return filtered_chunks
 
 def export_chunk (chunk, number, name):
@@ -70,15 +87,71 @@ def export_chunk (chunk, number, name):
     write_address = os.path.join("C:\\Users\\Seth\\Documents\\school\\EGR334\\exports\\", filename)
     write(write_address, chunk_array[number].sample_rate, chunk)  # Saving it to the file.
 
-def play_chunk (chunk, sample_rate, audio_device):
-        sd.play(chunk.data, sample_rate, device=4)
+def play_chunk (chunk, audio_device):
+        sd.play(chunk.data, chunk.sample_rate, device=audio_device)
         #stream = sd.OutputStream(samplerate=chunk.sample_rate, device=audio_device, channels=1)
         sd.wait()
 
-def play_chunk_array (chunk_array, sample_rate, audio_device):
+def play_chunk_array (chunk_array, audio_device):
     for n in range(len(chunk_array)):
-        play_chunk(chunk_array[n], sample_rate, audio_device)
+        play_chunk(chunk_array[n], audio_device)
 
+def async_play_chunks (chunk_array_index, audio_device):
+    i = 1
+    global global_chunk_array
+    global lock
+    while True:
+        if not lock:
+            rows, columns = global_chunk_array.shape
+            #print("Num rows: " + str(rows))
+            if i < rows:
+                print("Num rows: " + str(rows))
+                print("I is: " + str(i))
+                play_chunk(global_chunk_array[chunk_array_index][i], audio_device)
+                i += 1
+
+if __name__ == '__main__':
+    lock = threading.Lock()
+    global_chunk_array = np.empty(shape=(1,3))
+    threads = []  # list to hold threads
+    chunk_array = import_wav('C:\\Users\\Seth\\Documents\\school\\EGR334\\audio\\Chicago.wav', 10000)
+    high_chunks = []
+    band_chunks = []
+    low_chunks = []
+
+    low = threading.Thread(target=async_play_chunks, args=(2, 4))
+    threads.append(low)
+    band = threading.Thread(target=async_play_chunks, args=(1, 4))
+    threads.append(band)
+    high = threading.Thread(target=async_play_chunks, args=(0, 4))
+    threads.append(high)
+    for thread in threads:
+        thread.start()
+    for thread in threads:  # wait for all threads to finish
+        thread.join()
+    x = 1
+    while x:
+        for n in range(len(chunk_array)):
+            #[high_chunk, band_chunk, low_chunk] = filter_chunk(chunk_array[n])
+            chunks = np.array(filter_chunk(chunk_array[n]), ndmin=2)
+            #print(chunks.shape)
+            #(global_chunk_array.shape)
+            #high_chunks.append(high_chunk)
+            #band_chunks.append(band_chunk)
+            #low_chunks.append(low_chunk)
+            lock.aquire()
+            global_chunk_array = np.append(global_chunk_array,chunks,axis=0)
+            lock.release()
+            rows, columns = global_chunk_array.shape
+            print(rows)
+            # export_chunk(high_chunk,n,"high")
+            # export_chunk(band_chunk, n,"band")
+            # export_chunk(low_chunk, n,"low")
+            x = 0
+        #print(global_chunk_array)
+
+
+"""
 if __name__ == '__main__':
     threads = []  # list to hold threads
     chunk_array = import_wav('C:\\Users\\Seth\\Documents\\school\\EGR334\\audio\\Chicago.wav', 10000)
@@ -93,13 +166,14 @@ if __name__ == '__main__':
         # export_chunk(high_chunk,n,"high")
         # export_chunk(band_chunk, n,"band")
         # export_chunk(low_chunk, n,"low")
-    low = threading.Thread(target=play_chunk_array, args=(low_chunks, chunk_array[n].sample_rate, 4))
+    low = threading.Thread(target=play_chunk_array, args=(low_chunks, 4))
     threads.append(low)
-    band = threading.Thread(target=play_chunk_array, args=(band_chunks, chunk_array[n].sample_rate, 4))
+    band = threading.Thread(target=play_chunk_array, args=(band_chunks, 4))
     threads.append(band)
-    high = threading.Thread(target=play_chunk_array, args=(high_chunks, chunk_array[n].sample_rate, 4))
+    high = threading.Thread(target=play_chunk_array, args=(high_chunks, 4))
     threads.append(high)
     for thread in threads:
         thread.start()
     for thread in threads:  # wait for all threads to finish
         thread.join()
+"""
